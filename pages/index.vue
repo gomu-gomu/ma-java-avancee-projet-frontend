8ababd128102d9e3a217f0be181748dbd5b8a59e
eoussama <oussama.essamadi@gmail.com>
2024-04-26 15:10:21 +0100

    remaining dashboards

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import Header from '~/components/shared/Header.vue';
 import AdminDashboard from '~/components/home/admin/AdminDashboard.vue';
+import ParentDashboard from '~/components/home/parent/ParentDashboard.vue';
+import TeacherDashboard from '~/components/home/teacher/TeacherDashboard.vue';
 import StudentDashboard from '~/components/home/student/StudentDashboard.vue';
 
 import { UserType } from '~/core/enums/user-type.enum';
@@ -14,6 +16,8 @@ import { UserHelper } from '~/core/helpers/user.helper';
 
       <UDashboardPanelContent>
         <AdminDashboard v-if="UserHelper.allowFor(UserType.Admin)" />
+        <ParentDashboard v-if="UserHelper.allowFor(UserType.Parent)" />
+        <TeacherDashboard v-if="UserHelper.allowFor(UserType.Teacher)" />
         <StudentDashboard v-if="UserHelper.allowFor(UserType.Student)" />
       </UDashboardPanelContent>
     </UDashboardPanel>
